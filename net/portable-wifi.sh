10d55cc5a14537e1f7fadb6c8b61f6fced1309ba
Carlos Mecha <mecha.carlos@gmail.com>
2013-06-06 08:42:56 +0200

    Add portable hotspot scripts.

---
+++ net/portable-wifi.sh
@@ -0,0 +1,37 @@
+#
+# Simple WIFI portable Hotspot.
+#
+
+# Configuration file.
+CONF="./portable-wifi-hostapd.conf";
+
+# IP Address
+IP="192.168.1.1";
+
+# Out interface
+OUT_IFACE="wlan0";
+
+# In interface
+IN_IFACE="eth0";
+
+# Shutting down.
+if [ "$1" == "-r" ]; then {
+  killall hostapd;
+  iwconfig $OUT_IFACE mode Managed;
+  echo "0" > /proc/sys/net/ipv4/ip_forward;
+  iptables -t nat -D POSTROUTING -o $IN_IFACE -j MASQUERADE;
+  exit 0;
+} fi;
+
+iwconfig $OUT_IFACE mode Master;
+ifconfig $OUT_IFACE $IP;
+echo "1" > /proc/sys/net/ipv4/ip_forward;
+iptables -t nat -A POSTROUTING -o $IN_IFACE -j MASQUERADE;
+hostapd $CONF 2>&1 >> /dev/null &
+
+unset CONF;
+unset IP;
+unset IN_IFACE;
+unset OUT_IFACE;
+
+exit 0;
